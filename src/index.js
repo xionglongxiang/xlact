119d99c63fc4647c97809f002a05f93c261f4aa9
xionglongxiang <xionglongxiang@126.com>
2022-11-13 15:17:28 +0800

    replace render function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
 import './index.css'
 // import App from './App';
 import reportWebVitals from './reportWebVitals'
@@ -12,10 +10,24 @@ import reportWebVitals from './reportWebVitals'
 // );
 
 // const element = <h1 title="foo">Hello</h1>
-const element = React.createElement('h1', { title: 'foo' }, 'Hello')
+// const element = React.createElement('h1', { title: 'foo' }, 'Hello')
+const element = {
+  type: 'h1',
+  props: {
+    title: 'foo',
+    children: 'Hello',
+  },
+}
 const container = document.getElementById('root')
 
-ReactDOM.render(element, container)
+const node = document.createElement(element.type)
+node['title'] = element.props.title
+const text = document.createTextNode('')
+text['nodeValue'] = element.props.children
+node.appendChild(text)
+container.appendChild(node)
+
+// ReactDOM.render(element, container)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
