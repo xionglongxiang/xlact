709f9276f6e5be25a3df42539fe4cafbabd733a2
xionglongxiang <xionglongxiang@126.com>
2022-11-13 15:13:56 +0800

    use createElement

--- src/index.js
+++ src/index.js
@@ -11,7 +11,8 @@ import reportWebVitals from './reportWebVitals'
 //   </React.StrictMode>
 // );
 
-const element = <h1 title="foo">Hello</h1>
+// const element = <h1 title="foo">Hello</h1>
+const element = React.createElement('h1', { title: 'foo' }, 'Hello')
 const container = document.getElementById('root')
 
 ReactDOM.render(element, container)
